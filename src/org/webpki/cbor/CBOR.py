4694b8249125ddfd8ea050c774cc94286108a4c5
Anders Rundgren <anders.rundgren.net@gmail.com>
2026-02-02 11:52:17 +0100

    Update CBOR.py

--- src/org/webpki/cbor/CBOR.py
+++ src/org/webpki/cbor/CBOR.py
@@ -1,23 +1,25 @@
-import binascii, struct, math
+import binascii, struct, math, io
 
 class CBOR:
 
-  _MT_UNSIGNED     = 0x00
-  _MT_NEGATIVE     = 0x20
-  _MT_BYTES        = 0x40
-  _MT_STRING       = 0x60
-  _MT_ARRAY        = 0x80
-  _MT_MAP          = 0xa0
-  _MT_SIMPLE       = 0xe0
-  _MT_TAG          = 0xc0
-  _MT_BIG_UNSIGNED = 0xc2
-  _MT_BIG_NEGATIVE = 0xc3
-  _MT_FALSE        = 0xf4
-  _MT_TRUE         = 0xf5
-  _MT_NULL         = 0xf6
-  _MT_FLOAT16      = 0xf9
-  _MT_FLOAT32      = 0xfa
-  _MT_FLOAT64      = 0xfb
+  _MT_UNSIGNED      = 0x00
+  _MT_NEGATIVE      = 0x20
+  _MT_BYTES         = 0x40
+  _MT_STRING        = 0x60
+  _MT_ARRAY         = 0x80
+  _MT_MAP           = 0xa0
+  _MT_TAG           = 0xc0
+  _MT_SIMPLE        = 0xe0
+
+  _TAG_BIG_UNSIGNED = 0xc2
+  _TAG_BIG_NEGATIVE = 0xc3
+
+  _SIMPLE_FALSE     = 0xf4
+  _SIMPLE_TRUE      = 0xf5
+  _SIMPLE_NULL      = 0xf6
+  _SIMPLE_FLOAT16   = 0xf9
+  _SIMPLE_FLOAT32   = 0xfa
+  _SIMPLE_FLOAT64   = 0xfb
 
   class Exception(Exception):
     def __init__(self, msg):
@@ -65,8 +67,8 @@ class CBOR:
         modifier += 1
       return bytearray([tag | modifier]) + array
     # True "bigint".
-    return bytearray([CBOR._MT_BIG_UNSIGNED if neg == 0 
-                      else CBOR._MT_BIG_NEGATIVE]) + CBOR._encode_string(CBOR._MT_BYTES, array)
+    return bytearray([CBOR._TAG_BIG_UNSIGNED if neg == 0 
+                      else CBOR._TAG_BIG_NEGATIVE]) + CBOR._encode_string(CBOR._MT_BYTES, array)
 
   @staticmethod
   def _int_range_check(value, min, max):
@@ -84,7 +86,7 @@ class CBOR:
   def _check_int_argument(value):
     return CBOR._check_argument_type(value, 'int')
 
-  class CborObject:
+  class _CborObject:
     def __init__(self):
       self.readFlag = False
 
@@ -112,33 +114,37 @@ class CBOR:
 
     def encode(self):
       return self._internal_encode()
+    
+    def check_for_unread(self):
+      if not self.readFlag:
+        CBOR._error("Not read: " + type(self).__name__)
 
   ############
   #   Int    #
   ############
-  class Int(CborObject):
+  class Int(_CborObject):
     def __init__(self, value):
       super().__init__()
-      self.value = CBOR._check_int_argument(value)
+      self._value = CBOR._check_int_argument(value)
 
     def _internal_encode(self):
-      return CBOR._encode_integer(CBOR._MT_UNSIGNED, self.value)
+      return CBOR._encode_integer(CBOR._MT_UNSIGNED, self._value)
     
     def _get(self):
-      return self.value
+      return self._value
   
   ############
   #  Float   #
   ############
-  class Float(CborObject):
+  class Float(_CborObject):
     def __init__(self, value):
       super().__init__()
       if type(value).__name__ == 'int':
         value = float(value)
-      self.value = CBOR._check_argument_type(value, 'float')
+      self._value = CBOR._check_argument_type(value, 'float')
       u8 = bytearray(struct.pack('!d', value))
       print(binascii.hexlify(u8))
-      if math.isfinite(value) == False:
+      if not math.isfinite(value):
         CBOR._error("NF Not implemented")
       if value == 0:
         CBOR._error("0 Not implemented")
@@ -147,43 +153,71 @@ class CBOR:
       return CBOR._encode_integer(0x00, 5)
     
     def _get(self):
-      return self.value
+      return self._value
   
   ############
   #  String  #
   ############
-  class String(CborObject):
+  class String(_CborObject):
     def __init__(self, string):
       super().__init__()
-      self.string = CBOR._check_argument_type(string, 'str')
+      self._string = CBOR._check_argument_type(string, 'str')
 
     def _internal_encode(self):
-      return CBOR._encode_string(CBOR._MT_STRING, self.string.encode("utf8"))
+      return CBOR._encode_string(CBOR._MT_STRING, self._string.encode("utf8"))
   
     def _get(self):
-      return self.string
+      return self._string
     
   ############
   #  Array   #
   ############
-  class Array(CborObject):
+  class Array(_CborObject):
     def __init__(self):
       super().__init__()
-      self.objects = list()
+      self._objects = list()
 
     def _internal_encode(self):
-      encoded = CBOR._encode_integer(CBOR._MT_ARRAY, len(self.objects))
-      for object in self.objects:
+      encoded = CBOR._encode_integer(CBOR._MT_ARRAY, len(self._objects))
+      for object in self._objects:
         encoded += object._internal_encode()
       return encoded
     
     def add(self, object):
-      self.objects.append(object)
+      self._objects.append(object)
       return self
+
+  ################  
+  #   Decoding   #
+  ################
+  class _Decoder:
+    def __init__(self, cbor_stream, max_length):
+      CBOR._check_int_argument(max_length)
+      if type(cbor_stream).__name__ != 'BytesIO':
+        CBOR._error("Unexpected stream type: " + type(cbor_stream).__name__)
+      self._cbor_stream = cbor_stream
+      self._max_length = max_length
+      self._current = 0
   
+    def decode_with_options(self):
+        while (chunk := self._cbor_stream.read(1)):
+          self._current += 1
+        print(str(self._current))
+
   @classmethod
   def decode(cls, cbor):
-    print(cbor)
+    if type(cbor).__name__ not in ['bytes', 'bytearray']:
+      CBOR._error("Unexpected CBOR argument: " + type(cbor).__name__)
+    CBOR.init_decoder(io.BytesIO(cbor), len(cbor)).decode_with_options()
+
+  @classmethod
+  def init_decoder(cls, cbor_stream, max_length):
+    return CBOR._Decoder(cbor_stream, max_length)
+
+
+##########################
+#       Testing...       #
+##########################
 
 i = CBOR.Int(50)
 print(binascii.hexlify(i.encode()))
@@ -193,7 +227,7 @@ print(i.get_int8())
 s = CBOR.String("kurt€")
 print(binascii.hexlify(s.encode()))
 
-print(s.get_int8())
+# print(s.get_int8())
 
 a = CBOR.Array()
 a.add(i).add(s)
@@ -203,4 +237,8 @@ f = CBOR.Float(2.0e50)
 print(f.get_float64())
 print(binascii.hexlify(f.encode()))
 
-CBOR.decode("DEC")
+k = CBOR.Int(7)
+k.get_int8()
+k.check_for_unread()
+
+CBOR.decode("DEC".encode("utf8"))
